f0a84c76d7d18cc30d625c1afb1df8239f893d4f
Ryan Duong <ryan.duong@aol.co.uk>
2025-05-03 23:32:43 +0100

    Initial commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "javascript", "c", "markdown" },
+                highlight = { enable = true },  -- (was misspelled as 'higlight' earlier btw)
+                indent = { enable = true },
+            })
+        end,
+    },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "dracula",
+                },
+            })
+        end,
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- optional but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        lazy = false, -- neo-tree loads immediately at startup
+        config = function()
+            -- Keymaps specific to Neo-tree
+            vim.keymap.set("n", "<C-n>", ":Neotree toggle right<CR>")
+            vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left window" })
+            vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right window" })
+        end,
+    },
+}
