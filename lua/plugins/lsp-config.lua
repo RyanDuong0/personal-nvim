32558d2be7d58bda077c98d0cef0f1706bf30341
Ryan Duong <ryan.duong@aol.co.uk>
2025-05-07 09:03:53 +0100

    Configured python, debugger, auto formatting

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,15 @@
-return{
+return {
     {
         "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "pyright",
+                "ruff",
+                "mypy",
+                "black",
+                "debugpy",
+            },
+        },
         config = function()
             require("mason").setup()
         end,
@@ -16,20 +25,21 @@ return{
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.ts_ls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+                filetypes = { "python" },
             })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end
-    },
-    {
-        "mfussenegger/nvim-jdtls",
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
     },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,14 +13,30 @@ return {
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.jdtls,
 
                 -- Diagnostics sources
                 null_ls.builtins.diagnostics.stylelint,
+                require("none-ls.diagnostics.ruff"),
+                null_ls.builtins.diagnostics.mypy,
 
                 -- Formatting for stylelint
                 null_ls.builtins.formatting.stylelint,
             },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({
+                        group = augroup,
+                        buffer = bufnr,
+                    })
+                    vim.api.nvim_create_autocmd("BufWritePre",{
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({bufnr = buffnr})
+                        end,
+                    })
+                end
+            end
         })
         vim.keymap.set("n", "<leader>gf", function()
             vim.lsp.buf.format({ async = true })
