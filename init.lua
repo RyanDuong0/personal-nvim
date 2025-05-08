8b3523e2acc121198787707d0608167650e781ff
Ryan Duong <ryan.duong@aol.co.uk>
2025-05-08 21:51:02 +0100

    Added transparent background support

--- init.lua
+++ init.lua
@@ -1,29 +1,33 @@
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.ex)
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+vim.cmd([[ hi normal guibg=none ctermbg=none ]])
+vim.cmd([[ hi normalnc guibg=none ctermbg=none ]])
+vim.cmd([[ hi pmenu guibg=none ctermbg=none ]])
+
 vim.opt.termguicolors = true
 
-vim.keymap.set('n', '<leader>mr', function()
-    vim.cmd([[%s/\r$//]])
-end, { desc = 'Removes leading ^M' })
+vim.keymap.set("n", "<leader>mr", function()
+	vim.cmd([[%s/\r$//]])
+end, { desc = "removes leading ^m" })
 
--- Bootstrap lazy.nvim
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "failed to clone lazy.nvim:\n", "errormsg" },
+			{ out, "warningmsg" },
+			{ "\npress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -33,10 +37,11 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup("plugins")
 require("vim-options")
 require("autopairs-config")
-require("bufferline").setup{}
+require("bufferline").setup({})
 require("bufferline-remap")
 
+require("catppuccin").setup({
+	transparent_background = true,
+})
+
 vim.cmd.colorscheme("catppuccin")
-
-
-
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,67 +1,66 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "dracula"
-                },
-            })
-        end,
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- optional but recommended
-            "MunifTanjim/nui.nvim",
-        },
-        lazy = false, -- neo-tree loads immediately at startup
-        config = function()
-            -- Keymaps specific to Neo-tree
-            vim.keymap.set("n", "<C-n>", ":Neotree toggle right<CR>")
-            vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left window" })
-            vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right window" })
-        end,
-    },
-    {
-        "tpope/vim-commentary",
-    },
-    {
-        'nvimdev/dashboard-nvim',
-        event = 'VimEnter',
-        config = function()
-            require('dashboard').setup {
-            }
-        end,
-        dependencies = { {'nvim-tree/nvim-web-devicons'}}
-    },
-    {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = "nvim-tree/nvim-web-devicons",
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",  -- updated API
-        opts = {
-            indent = {
-                char = "│",  -- or try "▏", "▎", "┆", etc.
-            },
-            scope = {
-                enabled = true,  -- this draws the scope indicator
-                show_start = false,
-                show_end = false,
-            },
-            exclude = {
-                filetypes = { "help", "dashboard", "lazy", "NvimTree" }
-            }
-        }
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "dracula",
+				},
+			})
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false, -- neo-tree loads immediately at startup
+		config = function()
+			-- Keymaps specific to Neo-tree
+			vim.keymap.set("n", "<C-n>", ":Neotree toggle right<CR>")
+			vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left window" })
+			vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right window" })
+		end,
+	},
+	{
+		"tpope/vim-commentary",
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl", -- updated API
+		opts = {
+			indent = {
+				char = "│", -- or try "▏", "▎", "┆", etc.
+			},
+			scope = {
+				enabled = true, -- this draws the scope indicator
+				show_start = false,
+				show_end = false,
+			},
+			exclude = {
+				filetypes = { "help", "dashboard", "lazy", "NvimTree" },
+			},
+		},
+	},
 }
