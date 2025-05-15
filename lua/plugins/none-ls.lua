2599d928a2fb841430f5e2e0a13ed67710a1fd66
Ryan Duong <ryan.duong@aol.co.uk>
2025-05-15 16:52:55 +0100

    Added React LSP

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,45 +1,53 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- Formatting sources
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.clang_format,
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-				-- Diagnostics sources
-				null_ls.builtins.diagnostics.stylelint,
-				require("none-ls.diagnostics.ruff"),
-				null_ls.builtins.diagnostics.mypy,
+        null_ls.setup({
+            sources = {
+                -- Formatting
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier.with({
+                    filetypes = {
+                        "javascript",
+                        "javascriptreact",
+                        "typescript",
+                        "typescriptreact",
+                        "json",
+                        "css",
+                        "html",
+                    },
+                }),
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.stylelint,
 
-				-- Formatting for stylelint
-				null_ls.builtins.formatting.stylelint,
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({
-						group = augroup,
-						buffer = bufnr,
-					})
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = buffnr })
-						end,
-					})
-				end
-			end,
-		})
-		vim.keymap.set("n", "<leader>gf", function()
-			vim.lsp.buf.format({ async = true })
-		end, { desc = "LSP Format" })
-	end,
+                -- Diagnostics
+                null_ls.builtins.diagnostics.stylelint,
+                require("none-ls.diagnostics.ruff"),
+                null_ls.builtins.diagnostics.mypy,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
+        })
+
+        vim.keymap.set("n", "<leader>gf", function()
+            vim.lsp.buf.format({ async = true })
+        end, { desc = "LSP Format" })
+    end,
 }
